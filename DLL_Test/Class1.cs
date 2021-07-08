46cec6417e9ebb27b98c01bcced7fd2462041655
Beyourwatch <beyourwatch@163.com>
2021-07-08 14:59:19 +0800

    add code

---
+++ DLL_Test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace DLL_Test
+{
+    public class tmp
+    {
+        private int var1 = 0;
+
+        public int var { get; set; }
+
+        public void pirntvar()
+        {
+            var = var1;
+            Console.WriteLine("var = " + (var+1).ToString());
+        }
+
+    }
+}
